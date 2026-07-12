aca505155d7111c0d8b6e1c7f8316d8016053870
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
-output "spring_cloud_connections" {
-  description = "All spring_cloud_connection resources"
-  value       = azurerm_spring_cloud_connection.spring_cloud_connections
-  sensitive   = true
-}
 output "spring_cloud_connections_authentication" {
-  description = "List of authentication values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.authentication]
+  description = "Map of authentication values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.authentication }
   sensitive   = true
 }
 output "spring_cloud_connections_client_type" {
-  description = "List of client_type values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.client_type]
+  description = "Map of client_type values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.client_type }
 }
 output "spring_cloud_connections_name" {
-  description = "List of name values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.name]
+  description = "Map of name values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.name }
 }
 output "spring_cloud_connections_secret_store" {
-  description = "List of secret_store values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.secret_store]
+  description = "Map of secret_store values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.secret_store }
 }
 output "spring_cloud_connections_spring_cloud_id" {
-  description = "List of spring_cloud_id values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.spring_cloud_id]
+  description = "Map of spring_cloud_id values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.spring_cloud_id }
 }
 output "spring_cloud_connections_target_resource_id" {
-  description = "List of target_resource_id values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.target_resource_id]
+  description = "Map of target_resource_id values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.target_resource_id }
 }
 output "spring_cloud_connections_vnet_solution" {
-  description = "List of vnet_solution values across all spring_cloud_connections"
-  value       = [for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : v.vnet_solution]
+  description = "Map of vnet_solution values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.vnet_solution }
 }
 
