cc038a389943ef43a9f833357429013841cae9b0
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:16:57 +0100

    Update to schema version v5.0.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "spring_cloud_connections_id" {
 }
 output "spring_cloud_connections_authentication" {
   description = "Map of authentication values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
-  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.authentication if v.authentication != null && length(v.authentication) > 0 }
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => one(v.authentication) if v.authentication != null && length(v.authentication) > 0 }
   sensitive   = true
 }
 output "spring_cloud_connections_client_type" {
@@ -17,7 +17,7 @@ output "spring_cloud_connections_name" {
 }
 output "spring_cloud_connections_secret_store" {
   description = "Map of secret_store values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
-  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => v.secret_store if v.secret_store != null && length(v.secret_store) > 0 }
+  value       = { for k, v in azurerm_spring_cloud_connection.spring_cloud_connections : k => one(v.secret_store) if v.secret_store != null && length(v.secret_store) > 0 }
 }
 output "spring_cloud_connections_spring_cloud_id" {
   description = "Map of spring_cloud_id values across all spring_cloud_connections, keyed the same as var.spring_cloud_connections"
